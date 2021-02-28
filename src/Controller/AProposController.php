dd620ee7b19138f155b5262d3822478101c19e93
Smaga Michaele <smaga.michael@bbox.fr>
2021-02-28 16:18:25 +0100

    front fait

---
+++ src/Controller/AProposController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AProposController extends AbstractController
+{
+    /**
+     * @Route("/a/propos", name="a_propos")
+     */
+    public function index(): Response
+    {
+        return $this->render('a_propos/index.html.twig', [
+            'controller_name' => 'AProposController',
+        ]);
+    }
+}
