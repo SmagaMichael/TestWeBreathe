de8480a8ef6c3232e06ab92fe7d00f1e000f415c
Smaga Michaele <smaga.michael@bbox.fr>
2021-03-05 18:47:24 +0100

    commit avant l'ajout des catégories

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+
+$('#add_module_form_temperature').after('<div id="result">'+$('#add_module_form_temperature').val()+'°C</div>')
+
+$('#add_module_form_temperature').on('input', function() {
+
+    $('#result').remove();
+   //je récupere la valeur du input et l'ajoute directement en dessous de celui-ci
+    $(this).after('<div id="result">'+$(this).val()+'°C</div>')
+});
+
+
+
+$('#add_module_form_duree_fonctionnement').after('<div id="result2">'+$('#add_module_form_duree_fonctionnement').val()+'H</div>')
+
+$('#add_module_form_duree_fonctionnement').on('input', function() {
+
+    $('#result2').remove();
+    $(this).after('<div id="result2">'+$(this).val()+'H</div>')
+});
--- src/Form/AddModuleFormType.php
+++ src/Form/AddModuleFormType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Module;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,12 +18,39 @@ class AddModuleFormType extends AbstractType
             ->add('name')
             ->add('number')
             ->add('description')
-            ->add('type')
-            ->add('temperature')
-            ->add('duree_fonctionnement')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                  //'Affiche' => 'Value',
+                    'Montre' => 1,
+                    'Chauffage' => 2,
+                    'Prise' => 3,
+                    'Assistant Vocal' => 4,
+                    'Caméra' => 5,
+                ],
+            ])
+
+            ->add('temperature', RangeType::class,[
+                'attr' => [
+                    'min' => 0,
+                    'max' => 30,
+                    'class' => 'p-0',
+                ]
+            ])
+
+            ->add('duree_fonctionnement', RangeType::class,[
+                'attr' => [
+                    'min' => 0,
+                    'max' => 24,
+                    'class' => 'p-0',
+                ]
+            ])
             ->add('donnees_envoyees')
             ->add('etat_de_marche')
-            ->add('image', FileType::class, [])
+
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'required' => false
+            ])
                 
         ;
     }
